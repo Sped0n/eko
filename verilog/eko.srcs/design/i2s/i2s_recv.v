3b92ffc465dcc688cfa24f11bf6eb65e1ee8b0fb
Spedon <realsped0n@outlook.com>
2024-01-02 22:21:21 +0800

    refactor: refactor I2S clock and data logic to improve timing accuracy
    - Remove the `i2s_ready` output from being part of the rising-edge-triggered clk block
    - Change the `i2s_ready` output to be set at the correct clock cycle
    - Update the logic for shifting `i2s_data` based on `half_bit_count` and `i2s_bclk`

--- verilog/eko.srcs/design/i2s/i2s_recv.v
+++ verilog/eko.srcs/design/i2s/i2s_recv.v
@@ -21,144 +21,58 @@
 
 
 module i2s_recv (
-    input             clk,        // main clock, will be divided by param
+    input             clk,        // main clock
     input             rst_n,      // reset
     input             i2s_din,    // TODO: change from 1 channel to 6 channel matrix
-    output reg        i2s_lrclk,
-    output reg        i2s_bclk,
-    output reg        i2s_ready,
-    output reg [31:0] i2s_data    // left 16 bit, right 16 bit
+    output            i2s_lrclk,
+    output            i2s_bclk,
+    output reg        i2s_ready,  // main clock domain, high valid
+    output reg [31:0] i2s_data    // main clock domain, {left 16 bit, right 16 bit}
 );
 
   // *** parameter define ***
-  parameter DIVISOR = 16;  // 50MHz / 16 = 3.125MHz
-
-  localparam IDLE = 3'b001;
-  localparam LEFT = 3'b010;
-  localparam RIGHT = 3'b100;
+  parameter DIVISOR = 16;
 
   // *** reg define ***
-  reg [2:0] state, next_state;
-  reg [3:0] bclk_cnt;
-  reg [4:0] lrclk_cnt;
-  reg [5:0] cnt;
-  reg       lrclk_d0;
+  reg  [4:0] clock_count;
+  reg  [6:0] bit_count;
 
   // *** wire define ***
-  wire lrclk_negedge, lrclk_posedge;
+  wire [5:0] half_bit_count;
 
   // *** main code ***
+  assign i2s_bclk       = bit_count[0];
+  assign i2s_lrclk      = bit_count[6];
+  assign half_bit_count = bit_count[6:1];
 
-  // assign
-  assign lrclk_negedge = lrclk_d0 & ~i2s_lrclk;
-  assign lrclk_posedge = ~lrclk_d0 & i2s_lrclk;
-
-  // bclk
   always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-      bclk_cnt <= 0;
-      i2s_bclk <= 0;
+      i2s_ready   <= 0;
+      clock_count <= 0;
+      bit_count   <= 0;
+      i2s_data    <= 0;
     end else begin
-      bclk_cnt <= bclk_cnt + 1;
-      if (bclk_cnt == DIVISOR - 1) begin
-        i2s_bclk <= ~i2s_bclk;
-      end
-    end
-  end
-
-  // lrclk
-  always @(negedge i2s_bclk or negedge rst_n) begin
-    if (!rst_n) begin
-      lrclk_cnt <= 0;
-      i2s_lrclk <= 0;
-    end else begin
-      lrclk_cnt <= lrclk_cnt + 1;
-      if (lrclk_cnt == 5'd31) begin
-        i2s_lrclk <= ~i2s_lrclk;
-      end
-    end
-  end
-
-  // lrclk posedge
-  always @(posedge i2s_bclk or negedge rst_n) begin
-    if (!rst_n) begin
-      lrclk_d0 <= 1'b0;
-    end else begin
-      lrclk_d0 <= i2s_lrclk;
-    end
-  end
-
-  // *** state machine ***
-
-  // state transition
-  always @(posedge i2s_bclk or negedge rst_n) begin
-    if (!rst_n) begin
-      state <= LEFT;
-    end else begin
-      state <= next_state;
-    end
-  end
-
-  // state transition logic
-  always @(*) begin
-    next_state = state;
-    case (state)
-      IDLE: begin
-        if (lrclk_posedge == 1'b1) begin
-          next_state = RIGHT;
-        end else if (lrclk_negedge == 1'b1) begin
-          next_state = LEFT;
-        end else begin
-          next_state = IDLE;
-        end
-      end
-      LEFT: begin
-        if (cnt == 6'd16) begin
-          next_state = IDLE;
-        end else begin
-          next_state = LEFT;
-        end
-      end
-      RIGHT: begin
-        if (cnt == 6'd16) begin
-          next_state = IDLE;
-        end else begin
-          next_state = RIGHT;
-        end
-      end
-    endcase
-  end
-
-  // state machine output
-  always @(posedge i2s_bclk or negedge rst_n) begin
-    if (!rst_n) begin
-      i2s_ready <= 0;
-      i2s_data <= 0;
-      cnt <= 0;
-    end else begin
-      case (state)
-        IDLE: begin
-          if (i2s_ready == 1'b1) begin
-            i2s_ready <= 0;
-            i2s_data  <= 0;
-          end
-          cnt <= 0;
-        end
-        LEFT: begin
-          if (cnt < 6'd16) begin
-            i2s_data[31:16] <= {i2s_data[30:16], i2s_din};
-            cnt <= cnt + 1;
+      if (clock_count == DIVISOR - 1) begin
+        clock_count <= 0;
+        bit_count   <= bit_count + 1;
+        if (!i2s_bclk) begin
+          if (half_bit_count > 0 && half_bit_count < 17) begin  // left, cast to 16 bit
+            i2s_data[31:16] <= {i2s_data[30:16], i2s_din};  // shift
+          end else if (half_bit_count > 32 && half_bit_count < 49) begin  // right, cast to 16 bit
+            i2s_data[15:0] <= {i2s_data[14:0], i2s_din};  // shift
           end
         end
-        RIGHT: begin
-          if (cnt < 6'd16) begin
-            i2s_data[15:0] <= {i2s_data[14:0], i2s_din};
-            cnt <= cnt + 1;
-          end else begin
-            i2s_ready <= 1;
-          end
+        if (bit_count == 98) begin  // 98 = 49 * 2
+          // don't use half_bit_count here, because it contain 2 clock cycle, and will trigger i2s ready twice
+          i2s_ready <= 1;
+        end else if (bit_count == 99) begin
+          // same as above, don't clear the result twice
+          i2s_data <= 0;
         end
-      endcase
+      end else begin
+        clock_count <= clock_count + 1;
+        i2s_ready   <= 0;
+      end
     end
   end
 
