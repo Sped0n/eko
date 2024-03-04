2aed097aa6a70d4049ef788b8e8d9101cd5c57be
Sped0n <realsped0n@outlook.com>
2024-03-04 15:38:30 +0800

    feat: implement Python script for generating and plotting sine waves
    - Add Python script for generating sine waves and plotting their FFT
    - Plot original signed sine wave and FFT
    - Plot sine wave after converting signed number to unsigned number
    - Save plotted data to a file and display it

---
+++ python/sin_gen.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+Fs = 1024
+t = np.arange(0, 1, 1 / Fs)
+y = np.sin(10 * t)
+y_16bit = np.floor(y * np.power(2, 15))
+
+plt.figure()
+
+plt.subplot(221)
+plt.plot(y_16bit)
+plt.xlabel("Original signed sine wave")
+
+y1 = np.fft.fft(y_16bit, 512)
+
+plt.subplot(222)
+plt.plot(y1.real)
+plt.xlabel("FFT of original signed number (Real part)")
+
+y_16bit_a = y_16bit.copy()
+
+y_16bit_a[y_16bit_a < 0] += 2**16
+
+plt.subplot(223)
+plt.plot(y_16bit_a)
+plt.xlabel("Sine wave after converting signed number to unsigned number")
+
+y2 = np.fft.fft(y_16bit_a, 512)
+
+plt.subplot(224)
+plt.plot(y2.real)
+plt.xlabel("FFT after converting to unsigned number (Real part)")
+
+np.savetxt("sinwave.txt", y_16bit_a, fmt="%d")
+
+plt.show()
