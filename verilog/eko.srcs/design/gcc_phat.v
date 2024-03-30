cd74eaa7e413728ec008bed2e77ef99ca6a1029a
Spedon <realsped0n@outlook.com>
2024-03-30 23:25:36 +0800

    feat: add gcc_phat module

---
+++ verilog/eko.srcs/design/gcc_phat.v
@@ -0,0 +1,126 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: spedon wen
+// 
+// Create Date: 03/30/2024 09:03:30 PM
+// Design Name: 
+// Module Name: gcc_phat
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module gcc_phat (
+    input         aclk,
+    input         aresetn,
+    input  [31:0] s_axis_in_tdata,
+    input         s_axis_in_tvalid,
+    output        s_axis_in_tready,
+    output [15:0] m_axis_out_tdata,
+    output        m_axis_out_tvalid,
+    input         m_axis_out_tready
+);
+
+  wire [127:0] axis_xfft_0_tdata;
+  wire         axis_xfft_0_tvalid;
+  wire         axis_xfft_0_tready;
+
+  xfft_0 xfft_0_inst0 (
+      .aclk                (aclk),
+      .aresetn             (aresetn),
+      .s_axis_data_tdata   ({16'd0, s_axis_in_tdata[31:16], 16'd0, s_axis_in_tdata[15:0]}),
+      .s_axis_data_tready  (s_axis_in_tready),
+      .s_axis_data_tvalid  (s_axis_in_tvalid),
+      .s_axis_data_tlast   (0),
+      .s_axis_config_tdata ({6'd0, 1'b1, 1'b1}),
+      .s_axis_config_tvalid(1'b1),
+      .m_axis_data_tdata   (axis_xfft_0_tdata),
+      .m_axis_data_tready  (axis_xfft_0_tready),
+      .m_axis_data_tvalid  (axis_xfft_0_tvalid)
+  );
+
+  wire [111:0] axis_multiply_0_tdata;
+  wire         axis_multiply_0_tvalid;
+  wire         axis_multiply_0_tready;
+
+  multiply_0 multiply_0_inst0 (
+      .aclk         (aclk),
+      .aresetn      (aresetn),
+      .s_axis_tdata (axis_xfft_0_tdata),
+      .s_axis_tvalid(axis_xfft_0_tvalid),
+      .s_axis_tready(axis_xfft_0_tready),
+      .m_axis_tdata (axis_multiply_0_tdata),
+      .m_axis_tvalid(axis_multiply_0_tvalid),
+      .m_axis_tready(axis_multiply_0_tready)
+  );
+
+  wire [47:0] axis_scale_0_tdata;
+  wire        axis_scale_0_tvalid;
+  wire        axis_scale_0_tready;
+
+  scale_0 scale_0_inst0 (
+      .aclk         (aclk),
+      .aresetn      (aresetn),
+      .s_axis_tdata (axis_multiply_0_tdata),
+      .s_axis_tvalid(axis_multiply_0_tvalid),
+      .s_axis_tready(axis_multiply_0_tready),
+      .m_axis_tdata (axis_scale_0_tdata),
+      .m_axis_tvalid(axis_scale_0_tvalid),
+      .m_axis_tready(axis_scale_0_tready)
+  );
+
+  wire [31:0] axis_normalize_0_tdata;
+  wire        axis_normalize_0_tvalid;
+  wire        axis_normalize_0_tready;
+
+  normalize_0 normalize_0_inst0 (
+      .aclk         (aclk),
+      .aresetn      (aresetn),
+      .s_axis_tdata (axis_scale_0_tdata),
+      .s_axis_tvalid(axis_scale_0_tvalid),
+      .s_axis_tready(axis_scale_0_tready),
+      .m_axis_tdata (axis_normalize_0_tdata),
+      .m_axis_tvalid(axis_normalize_0_tvalid),
+      .m_axis_tready(axis_normalize_0_tready)
+  );
+
+  wire [31:0] axis_xfft_1_tdata;
+  wire        axis_xfft_1_tvalid;
+  wire        axis_xfft_1_tready;
+
+  xfft_1 xfft_1_inst0 (
+      .aclk                (aclk),
+      .aresetn             (aresetn),
+      .s_axis_data_tdata   (axis_normalize_0_tdata),
+      .s_axis_data_tvalid  (axis_normalize_0_tvalid),
+      .s_axis_data_tready  (axis_normalize_0_tready),
+      .s_axis_data_tlast   (0),
+      .s_axis_config_tdata ({5'b0, {9{2'b01}}, 1'b0}),
+      .s_axis_config_tvalid(1'b1),
+      .m_axis_data_tdata   (axis_xfft_1_tdata),
+      .m_axis_data_tvalid  (axis_xfft_1_tvalid),
+      .m_axis_data_tready  (axis_xfft_1_tready)
+  );
+
+  magnitude_0 magnitude_0_inst0 (
+      .aclk         (aclk),
+      .aresetn      (aresetn),
+      .s_axis_tdata (axis_xfft_1_tdata),
+      .s_axis_tvalid(axis_xfft_1_tvalid),
+      .s_axis_tready(axis_xfft_1_tready),
+      .m_axis_tdata (m_axis_out_tdata),
+      .m_axis_tvalid(m_axis_out_tvalid),
+      .m_axis_tready(m_axis_out_tready)
+  );
+
+endmodule
---
+++ verilog/eko.srcs/testbench/tb_gcc_phat.v
@@ -0,0 +1,90 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/30/2024 09:27:34 PM
+// Design Name: 
+// Module Name: tb_gcc_phat
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_gcc_phat ();
+  // *** parameter define ***
+  parameter PERIOD = 20;
+
+  // *** interger define ***
+  integer        sin_txt;
+  integer        count;
+
+
+  // *** reg define ***
+  reg            ready = 0;
+  reg            clk = 0;
+  reg            rst_n = 0;
+  reg     [15:0] sin_wave = 0;
+  reg            s_axis_data_tvalid = 0;
+  reg            s_axis_data_tlast = 0;
+
+  // *** wire define ***
+  wire           s_axis_data_tready;
+  wire    [15:0] axis_gcc_phat_tdata;
+  wire           axis_gcc_phat_tvalid;
+
+
+  // *** clock generator ***
+  initial begin
+    forever #(PERIOD / 2) clk = ~clk;
+  end
+
+  // *** reset ***
+  initial begin
+    #(PERIOD * 10) rst_n = 1;
+  end
+
+  // *** main code ***
+
+  // xfft instance
+  gcc_phat gcc_phat_inst0 (
+      .aclk(clk),
+      .aresetn(rst_n),
+      .s_axis_in_tdata({sin_wave, sin_wave}),
+      .s_axis_in_tready(s_axis_data_tready),
+      .s_axis_in_tvalid(s_axis_data_tvalid),
+      .m_axis_out_tdata(axis_gcc_phat_tdata),
+      .m_axis_out_tready(1'd1),
+      .m_axis_out_tvalid(axis_gcc_phat_tvalid)
+  );
+
+  // *** initial block ***
+  initial begin
+    count   = 0;
+    sin_txt = $fopen("C:\\Users\\spedon\\Documents\\eeworks\\FPGA\\eko\\python\\sinwave.txt", "r");
+    #10000 ready = 1;
+  end
+
+  // *** always block ***
+  always @(posedge clk) begin
+    if (s_axis_data_tready == 1 && ready == 1) begin
+      if (count < 1024) begin
+        s_axis_data_tvalid <= 1;
+        $fscanf(sin_txt, "%d", sin_wave);
+        count <= count + 1;
+      end else if (count == 1024) begin
+        $fclose(sin_txt);
+        s_axis_data_tvalid <= 0;
+      end
+    end
+  end
+endmodule
