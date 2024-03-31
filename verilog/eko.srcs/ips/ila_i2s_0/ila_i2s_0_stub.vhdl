d8b840dc543fd4ea62856ff2f09842d07c9a4af4
Spedon <realsped0n@outlook.com>
2024-04-01 00:39:38 +0800

    refactor: update ILA config

--- verilog/eko.srcs/ips/ila_i2s_0/ila_i2s_0_stub.vhdl
+++ verilog/eko.srcs/ips/ila_i2s_0/ila_i2s_0_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
--- Date        : Mon Jan  1 22:14:02 2024
+-- Date        : Mon Apr  1 00:36:14 2024
 -- Host        : Zen running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub
 --               c:/Users/spedon/Documents/eeworks/FPGA/eko/verilog/eko.srcs/ips/ila_i2s_0/ila_i2s_0_stub.vhdl
@@ -15,8 +15,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity ila_i2s_0 is
   Port ( 
     clk : in STD_LOGIC;
-    probe0 : in STD_LOGIC_VECTOR ( 31 downto 0 );
-    probe1 : in STD_LOGIC_VECTOR ( 0 to 0 )
+    probe0 : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    probe1 : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    probe2 : in STD_LOGIC_VECTOR ( 0 to 0 )
   );
 
 end ila_i2s_0;
@@ -25,7 +26,7 @@ architecture stub of ila_i2s_0 is
 attribute syn_black_box : boolean;
 attribute black_box_pad_pin : string;
 attribute syn_black_box of stub : architecture is true;
-attribute black_box_pad_pin of stub : architecture is "clk,probe0[31:0],probe1[0:0]";
+attribute black_box_pad_pin of stub : architecture is "clk,probe0[15:0],probe1[15:0],probe2[0:0]";
 attribute X_CORE_INFO : string;
 attribute X_CORE_INFO of stub : architecture is "ila,Vivado 2022.2";
 begin
