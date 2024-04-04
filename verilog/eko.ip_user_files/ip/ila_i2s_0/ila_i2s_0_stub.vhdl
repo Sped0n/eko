01621961666b329377890f71790211774d544932
Spedon <realsped0n@outlook.com>
2024-04-05 00:26:42 +0800

    refactor: change ILA config

--- verilog/eko.ip_user_files/ip/ila_i2s_0/ila_i2s_0_stub.vhdl
+++ verilog/eko.ip_user_files/ip/ila_i2s_0/ila_i2s_0_stub.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
--- Date        : Mon Apr  1 00:36:14 2024
+-- Date        : Thu Apr  4 22:49:13 2024
 -- Host        : Zen running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub
 --               c:/Users/spedon/Documents/eeworks/FPGA/eko/verilog/eko.srcs/ips/ila_i2s_0/ila_i2s_0_stub.vhdl
@@ -16,8 +16,10 @@ entity ila_i2s_0 is
   Port ( 
     clk : in STD_LOGIC;
     probe0 : in STD_LOGIC_VECTOR ( 15 downto 0 );
-    probe1 : in STD_LOGIC_VECTOR ( 15 downto 0 );
-    probe2 : in STD_LOGIC_VECTOR ( 0 to 0 )
+    probe1 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe2 : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    probe3 : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    probe4 : in STD_LOGIC_VECTOR ( 0 to 0 )
   );
 
 end ila_i2s_0;
@@ -26,7 +28,7 @@ architecture stub of ila_i2s_0 is
 attribute syn_black_box : boolean;
 attribute black_box_pad_pin : string;
 attribute syn_black_box of stub : architecture is true;
-attribute black_box_pad_pin of stub : architecture is "clk,probe0[15:0],probe1[15:0],probe2[0:0]";
+attribute black_box_pad_pin of stub : architecture is "clk,probe0[15:0],probe1[0:0],probe2[15:0],probe3[15:0],probe4[0:0]";
 attribute X_CORE_INFO : string;
 attribute X_CORE_INFO of stub : architecture is "ila,Vivado 2022.2";
 begin
--- verilog/eko.ip_user_files/sim_scripts/ila_i2s_0/modelsim/compile.do
+++ verilog/eko.ip_user_files/sim_scripts/ila_i2s_0/modelsim/compile.do
@@ -9,6 +9,7 @@ vmap xil_defaultlib modelsim_lib/msim/xil_defaultlib
 
 vlog -work xpm  -incr -mfcu  -sv "+incdir+../../../../eko.srcs/ips/ila_i2s_0/hdl/verilog" \
 "C:/Xilinx/Vivado/2022.2/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+"C:/Xilinx/Vivado/2022.2/data/ip/xpm/xpm_fifo/hdl/xpm_fifo.sv" \
 "C:/Xilinx/Vivado/2022.2/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
 
 vcom -work xpm  -93  \
